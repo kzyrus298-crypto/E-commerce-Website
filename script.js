132d45dc4e34eb28a4b5042074985affd66d7600
Karl Zyrus Pantaleon <kzyrus298@gmail.com>
2026-09-20 17:12:51 +0800

    Adding script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', function() {
+    var scrolled = window.scrollY;
+
+    document.querySelector('.phone-1').style.transform =
+    'translateY(' + (scrolled * 0.15) + 'px) rotate(-8deg)';
+    
+    document.querySelector('.phone-2').style.transform =
+    'translateY(' + (scrolled * -0.1) + 'px) rotate(8deg)';
+
+})
+
+document.addEventListener('click', function(){
+    document.getElementById('bgMusic').play();
+}, {once: true}) ;
